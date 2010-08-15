185e38b9d0f6838d50b74eb8999eec695e1bc4d3
Laurent Raufaste <analogue@glop.org>
2010-08-15 22:31:41 +0200

    Arrays are now generated dynamically, and the tests are now benchmarked

--- unit-testing.php
+++ unit-testing.php
@@ -1,11 +1,29 @@
 <?php
 
+define('NB_NUMBERS', 10000);
+
 require_once('sort.php');
 
+function display_time_spent($last_time, $message)
+{
+	$time_spent = number_format(microtime(true) - $last_time, 2);
+	echo "$message took $time_spent seconds\n";
+	
+	return microtime(true);
+}
+
+$time = microtime(true);
+
 // Setup data for the tests
-$sample_array = array(53, 914, 230, 785, 121, 350, 567, 631, 11, 827, 180);
+for ($i = 0; $i < NB_NUMBERS; $i++)
+{
+	$sample_array[] = rand();
+}
 $expected_array = $sample_array;
 sort($expected_array);
+
+$time = display_time_spent($time,
+	'Generating an array with '.NB_NUMBERS.' numbers');
 
 /**
  * The tests themselves
@@ -15,9 +33,11 @@ if (sortWithPHPBuiltin($sample_array) !== $expected_array)
 {
 	echo "Bad test for sortWithPHPBuiltin()\n";
 }
+$time = display_time_spent($time, 'sortWithPHPBuiltin()');
 
 if (sortByDirectSelection($sample_array) !== $expected_array)
 {
 	echo "Bad test for sortByDirectSelection()\n";
 	var_dump(sortByDirectSelection($sample_array));
 }
+$time = display_time_spent($time, 'sortByDirectSelection()');
